3fef180e8d16a8b18e1ac745154cdfc89703adb0
Anirudh Kala <122237860+anirudhkala110@users.noreply.github.com>
2024-04-23 11:31:56 +0530

    Update Details.jsx

--- frontend/src/Components/Products/Details.jsx
+++ frontend/src/Components/Products/Details.jsx
@@ -1,1 +1,115 @@
+import React from 'react'
+import arm1 from '../../Images/Arm1.jpeg'
+import arm2 from '../../Images/arm2.jpeg'
+import arm3 from '../../Images/arm3.jpeg'
+import arm4 from '../../Images/arm4.jpeg'
+import arm5 from '../../Images/arm5.jpeg'
 
+const data0 = [
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm1 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm2 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm3 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm4 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm5 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm1 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm2 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm3 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm4 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm5 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm1 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm2 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm3 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm4 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm5 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm1 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm2 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm3 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm4 },
+    { uploadedBy: 'Anirudh Kala', location: 'Delhi', machineName: 'Robotic Arm', img: arm5 },
+]
+const Products = () => {
+    return (
+        <div className='min-vh-100 d-flex justify-content-start align-items-start'>
+            <div>
+                <div className='sideBarButtonDetailsPage'>
+                    <button className=' btn btn-primary' type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample2" aria-controls="offcanvasExample2" style={{ position: 'fixed', zIndex: '100' }}>
+                        <i class="bi bi-arrow-left-right fw-bolder fs-5"></i>
+                    </button>
+                </div>
+                <div>
+                    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample2" aria-labelledby="offcanvasExampleLabel">
+                        <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Content</h5>
+                            <button type="button" class="btn-close text-reset fs-3" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <hr />
+                        {/* <div class="offcanvas-body overflow-auto pb-4" style={{    maxHeight: "-webkit-fill-available"}}> */}
+                        <div class="offcanvas-body overflow-auto pb-1" style={{ maxHeight: "80vh" }}>
+                            <div>
+                                <button className='btn btn-info w-100 mb-2'>
+                                    Types
+                                </button>
+                                <ul className='ps-0' style={{ listStyle: 'none' }}>
+                                    <li className='subList  py-1 ps-3  '>Army Mechanical Arms</li>
+                                    <li className='subList  py-1 ps-3  '>Medical Mechanical Arms</li>
+                                    <li className='subList  py-1 ps-3  '>School Mechanical Arms</li>
+                                    <li className='subList  py-1 ps-3  '>Construction Mechanical Arms</li>
+                                    <li className='subList  py-1 ps-3  '>Automobile Mechanical Arms</li>
+                                    <li className='subList  py-1 ps-3  '>Mechanical Arms for Mechanics</li>
+                                </ul>
+                            </div>
+                            <hr />
+                            <div>
+                                <button className='btn btn-info w-100 mb-2'>
+                                    Locations
+                                </button>
+                                <ul className='ps-0' style={{ listStyle: 'none' }}>
+                                    <li className='subList  py-1 ps-3  '>Delhi</li>
+                                    <li className='subList  py-1 ps-3  '>Mumbai</li>
+                                    <li className='subList  py-1 ps-3  '>Chandighar</li>
+                                    <li className='subList  py-1 ps-3  '>Maharastra</li>
+                                    <li className='subList  py-1 ps-3  '>Goa</li>
+                                    <li className='subList  py-1 ps-3  '>Uttarakhand</li>
+                                </ul>
+                            </div>
+                            <hr />
+                            <div>
+                                <button className='btn btn-info w-100 mb-2'>
+                                    Sortby
+                                </button>
+                                <ul className='ps-0' style={{ listStyle: 'none' }}>
+                                    <li className='subList  py-1 ps-3  '>A-Z</li>
+                                    <li className='subList  py-1 ps-3  '>Z-A</li>
+                                    <li className='subList  py-1 ps-3  '>Type</li>
+                                    <li className='subList  py-1 ps-3  '>Latest First</li>
+                                    <li className='subList  py-1 ps-3  '>Location</li>
+                                    <li className='subList  py-1 ps-3  '>Price</li>
+                                </ul>
+                            </div>
+                            <hr />
+                            {/* <div class="dropdown mt-3">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
+                                Dropdown button
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </div> */}
+                        </div>
+                    </div>
+                </div>
+              <div>
+                <div className='container'>
+                  edit it
+                </div>
+              </div>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Products
