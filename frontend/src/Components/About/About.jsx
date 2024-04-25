a0d6da15e44d73a2b33afc91a7b65005e43718fe
anirudhkala110 <anirudhkala110@gmail.com>
2024-04-26 01:45:41 +0530

    About Page Added

--- frontend/src/Components/About/About.jsx
+++ frontend/src/Components/About/About.jsx
@@ -2,7 +2,42 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='min-vh-100'>
+      <div className='container bg-white pt-3' style={{ borderLeft: '1px solid #8080806b', borderRight: '1px solid #8080806b' }}>
+        <center className='fs-4 fw-semibold alert alert-info rounded-0'>About</center>
+        <div>
+          <div>
+            At RoboShop, we're pioneering the frontier of robotics, one arm at a time. Our platform serves as the ultimate hub for all things robotic arms, catering to enthusiasts, professionals, and businesses seeking innovation and automation solutions.
+          </div>
+          <br />
+          <div>
+            Founded on a passion for technological advancement and a drive for excellence, RoboShop is dedicated to democratizing access to cutting-edge robotic arm technology. We believe that robotics holds the key to transforming industries, driving efficiency, and unlocking new possibilities across various sectors.
+          </div>
+          <br />
+          <div>
+            RoboShop stands apart as a curator of premium robotic arm solutions, sourced from leading manufacturers and suppliers worldwide. Our carefully curated selection encompasses a diverse range of robotic arms, from industrial-grade machines for manufacturing to precision tools for research and experimentation.
+          </div>
+          <br />
+          <div>
+            What sets RoboShop apart is our commitment to empowerment and education. Beyond providing access to state-of-the-art technology, we aim to inspire and educate our community about the transformative potential of robotics. Through informative content, tutorials, and resources, we empower individuals and businesses to harness the power of robotics and drive innovation in their respective fields.
+          </div>
+          <br />
+          <div>
+            Customer satisfaction is at the heart of everything we do at RoboShop. Our dedicated team is here to provide personalized support, guidance, and assistance at every step of your robotic arm journey. Whether you're a seasoned professional or a curious newcomer, we're here to ensure that your experience with RoboShop is seamless, rewarding, and inspiring.
+          </div>
+          <br />
+          <div>
+            Join us at RoboShop and embark on a journey of discovery, innovation, and transformation. Explore our vast selection of robotic arms, connect with fellow enthusiasts, and unlock the endless possibilities of robotics with RoboShop today!
+          </div>
+          <br />
+
+
+
+
+
+        </div>
+      </div>
+    </div>
   )
 }
 
